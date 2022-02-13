e5ff59184c5cd3ad3faa1313fd2f56cd7ac2b81e
ammar <ammarraneez@gmail.com>
2022-02-13 15:44:53 +0530

    feat: login ui

---
+++ components/Login.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="bg-black relative">
+      <h1>Login</h1>
+      <div className="flex flex-col absolute z-50 h-4/6 items-center w-full justify-center space-y-10">
+        <Image
+          src="https://avatars.githubusercontent.com/u/54928498?v=4"
+          height={200}
+          width={200}
+          className="object-cover rounded-full"
+        />
+        <button
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+        >
+          Login to the METAVERSE
+        </button>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://i.imgur.com/WYAjt3T.jpeg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Login;
