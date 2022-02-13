92ebc832da4e656fc75fe195d18b1ef16aa3b6ea
ammar <ammarraneez@gmail.com>
2022-02-13 17:12:32 +0530

    feat: create avatar and title username

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import React from 'react';
+import { useMoralis } from 'react-moralis';
+
+import Avatar from './Avatar';
+
+function Header() {
+  const { user } = useMoralis();
+
+  return (
+    <div>
+      <div className="text-pink-500">
+        {/* tailwind is mobile first - by default hidden, on larger screen inline-grid */}
+        <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+          <Image
+            src="https://avatars.githubusercontent.com/u/54928498?v=4"
+            layout="fill"
+            className="rounded-full"
+          />
+        </div>
+
+        <div>
+          <div className="h-48 w-48 relative lg:mx-auto border-pink-500 border-8 rounded-full">
+            <Avatar logoutOnPress />
+          </div>
+
+          <h1 className="text-3xl">
+            Welcome to the Metaverse
+          </h1>
+          <h2 className="text-5xl font-bold truncate">{user?.getUsername()}</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['i.imgur.com', 'avatars.githubusercontent.com']
+    domains: ['i.imgur.com', 'avatars.githubusercontent.com', 'avatars.dicebear.com']
   }
 }
