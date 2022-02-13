f9af5ce94dc6916c40083c2ee9c758b52d8c35c6
ammar <ammarraneez@gmail.com>
2022-02-13 18:42:16 +0530

    feat: send message

---
+++ components/SendMessage.tsx
@@ -0,0 +1,55 @@
+import React, { MouseEvent, useState } from 'react';
+import { useMoralis } from 'react-moralis';
+
+interface SendMessageProps {
+  endOfMessagesRef: any;
+}
+
+function SendMessage({ endOfMessagesRef }: SendMessageProps) {
+  const { user, Moralis } = useMoralis();
+  const [message, setMessage] = useState('');
+
+  const sendMessage = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!message) {
+      return;
+    }
+
+    const Messages = Moralis.Object.extend('Messages');
+    const messages = new Messages();
+
+    messages.save({
+      username: user?.getUsername(),
+      ethAddress: user?.get('ethAddress'),
+      message
+    }).then((message: string) => {
+
+    }, (err: Error) => {
+      console.log(err.message);
+    });
+
+    endOfMessagesRef.current.scrollIntoView({ behavior: 'smooth' });
+    setMessage('');
+  }
+
+  return (
+    <form className="flex fixed bottom-10 px-6 py-4 bg-black opacity-80 w-11/12 max-w-2xl shadow-xl rounded-full border-4 border-blue-400">
+      <input
+        type="text"
+        className="pr-5 flex-grow outline-none bg-transparent text-white placeholder-gray-500"
+        placeholder={`Enter a Message ${user?.getUsername()}...`}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        className="font-bold text-pink-500"
+        type="submit"
+        onClick={sendMessage}
+      >
+        Send
+      </button>
+    </form>
+  );
+}
+
+export default SendMessage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useMoralis } from 'react-moralis';
 
 import Header from '../components/Header';
 import Login from '../components/Login';
+import Messages from '../components/Messages';
 
 export default function Home() {
   const { isAuthenticated } = useMoralis();
@@ -20,6 +21,7 @@ export default function Home() {
 
       <div className="max-w-screen-2xl mx-auto">
         <Header />
+        <Messages />
       </div>
     </div>
   )
